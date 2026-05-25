aa052d216af3ae5da2484984dc3780f0928de102
Marcus Silva <devsilvamarcus@gmail.com>
2026-05-25 20:07:23 -0300

    feat: add api to integration a login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 import PasswordInput from '@/components/password-input';
@@ -22,6 +25,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import api from '@/lib/axios';
 
 const loginSchema = z.object({
   email: z
@@ -35,6 +39,19 @@ const loginSchema = z.object({
 });
 
 const LoginPage = () => {
+  const [user, setUser] = useState(null);
+
+  const loginMutation = useMutation({
+    mutationKey: ['login'],
+    mutationFn: async (data) => {
+      const response = await api.post('/users/login', {
+        email: data.email,
+        password: data.password,
+      });
+      return response.data;
+    },
+  });
+
   const methods = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -43,9 +60,52 @@ const LoginPage = () => {
     },
   });
 
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken');
+        const refreshToken = localStorage.getItem('refreshToken');
+
+        if (!accessToken || !refreshToken) {
+          return;
+        }
+
+        const response = await api.get('/users/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        setUser(response.data);
+      } catch (error) {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        console.error('Erro ao acessar os tokens no localStorage:', error);
+      }
+    };
+
+    init();
+  }, []);
+
   const handleSubmit = (data) => {
-    console.log('Dados de login:', data);
+    loginMutation.mutate(data, {
+      onSuccess: (loggedUser) => {
+        const accessToken = loggedUser.tokens.accessToken;
+        const refreshToken = loggedUser.tokens.refreshToken;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        toast.success('Login realizado com sucesso.');
+        setUser(loggedUser);
+      },
+      onError: () => {
+        toast.success('Erro ao executar login. Por favor, tente novamente.');
+      },
+    });
   };
+
+  if (user) {
+    return <h1 className="text-2xl">Bem-vindo, {user.first_name}!</h1>;
+  }
 
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
