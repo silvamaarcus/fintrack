9d4ba84b728440faadf2898c79dc69fa72a204e4
Marcus Silva <devsilvamarcus@gmail.com>
2026-05-25 18:54:34 -0300

    feat: add integration on singnup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 import PasswordInput from '@/components/password-input';
@@ -23,6 +26,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import api from '@/lib/axios';
 
 const signupSchema = z.object({
   firstName: z
@@ -53,6 +57,21 @@ const signupSchema = z.object({
 });
 
 const SignupPage = () => {
+  const [user, setUser] = useState(null);
+
+  const signupMutation = useMutation({
+    mutationKey: ['signup'],
+    mutationFn: async (data) => {
+      const response = await api.post('/users', {
+        first_name: data.firstName,
+        last_name: data.lastName,
+        email: data.email,
+        password: data.password,
+      });
+      return response.data;
+    },
+  });
+
   const methods = useForm({
     resolver: zodResolver(signupSchema),
     defaultValues: {
@@ -66,7 +85,21 @@ const SignupPage = () => {
   });
 
   const handleSubmit = (data) => {
-    console.log('Dados do formulário:', data);
+    signupMutation.mutate(data, {
+      onSuccess: (createdUser) => {
+        const accessToken = createdUser.token.accessToken;
+        const refreshToken = createdUser.token.refreshToken;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        setUser(createdUser);
+        toast.success('Conta criada com sucesso!');
+      },
+      onError: () => {
+        toast.error(
+          'Ocorreu um erro ao criar a conta. Por favor, tente novamente.',
+        );
+      },
+    });
   };
 
   return (
