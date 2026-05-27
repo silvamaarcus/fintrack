de9ce033d7e578923011f60c3b49b7231109fc74
Marcus Silva <devsilvamarcus@gmail.com>
2026-05-27 16:57:51 -0300

    refactor: standardize response format in UserService methods

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,11 @@
 import { protectedApi, publicApi } from '@/lib/axios';
 
-// Função para lidar com as requisições como: cadastro, login, me (dados do usuário logado)
+/*    
+    Adaptar os dados retornados para o formato esperado no frontend
+    (Ex: first_name -> firstName)
+*/
+
+// Service Layer para lidar com as requisições relacionadas ao usuário (signup, login, me)
 export const UserService = {
   signup: async (input) => {
     const response = await publicApi.post('/users', {
@@ -9,7 +14,13 @@ export const UserService = {
       email: input.email,
       password: input.password,
     });
-    return response.data;
+    return {
+      id: response.data.id,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      email: response.data.email,
+      tokens: response.data.tokens,
+    };
   },
 
   login: async (input) => {
@@ -17,11 +28,22 @@ export const UserService = {
       email: input.email,
       password: input.password,
     });
-    return response.data;
+    return {
+      id: response.data.id,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      email: response.data.email,
+      tokens: response.data.tokens,
+    };
   },
 
   me: async () => {
     const response = await protectedApi.get('/users/me');
-    return response.data;
+    return {
+      id: response.data.id,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      email: response.data.email,
+    };
   },
 };
