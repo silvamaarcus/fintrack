c358f633dd2c1b84b63df60f4c5eb844358e3dde
Marcus Silva <devsilvamarcus@gmail.com>
2026-05-27 10:09:56 -0300

    feat: ensure access token is attached on all api requests with interceptors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage';
+
 const api = axios.create({
   baseURL: '/api',
 });
 
 export default api;
+
+// Interceptor para adicionar o token de acesso a cada requisição
+api.interceptors.request.use((request) => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+  if (!accessToken) {
+    return request;
+  }
+  request.headers['Authorization'] = `Bearer ${accessToken}`;
+  return request;
+});
