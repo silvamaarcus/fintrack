2778299ef91f42232e0169fd4bb07c17e46d267e
Marcus Silva <devsilvamarcus@gmail.com>
2026-05-27 16:45:22 -0300

    refactor: add UserService login method

--- src/services/user.js
+++ src/services/user.js
@@ -1,10 +1,19 @@
 import { publicApi } from '@/lib/axios';
 
+// Função para lidar com as requisições como: cadastro, login, refresh token, etc.
 export const UserService = {
   signup: async (input) => {
     const response = await publicApi.post('/users', {
       first_name: input.firstName,
       last_name: input.lastName,
+      email: input.email,
+      password: input.password,
+    });
+    return response.data;
+  },
+
+  login: async (input) => {
+    const response = await publicApi.post('/users/login', {
       email: input.email,
       password: input.password,
     });
