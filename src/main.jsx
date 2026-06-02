15df50b2082ee8a06061a1405b1f5d634b707fbf
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-02 14:06:55 -0300

    chore: add react query dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import './index.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
@@ -28,6 +29,7 @@ createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </AuthContextProvider>
       <Toaster />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
